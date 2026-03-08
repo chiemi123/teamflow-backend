c38a5a7df1400bbc7662f38461f43f07c137628b
chiemi endo <milkrose0315@yahoo.co.jp>
2026-03-08 17:05:14 +0900

    feat: implement eloquent model relationships

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(TaskStatus::class);
+    }
+
+    public function assignedUser()
+    {
+        return $this->belongsTo(User::class, 'assigned_user_id');
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+    public function comments()
+    {
+        return $this->hasMany(TaskComment::class);
+    }
+
+    public function attachments()
+    {
+        return $this->hasMany(Attachment::class);
+    }
+
+    public function activityLogs()
+    {
+        return $this->hasMany(TaskActivityLog::class);
+    }
 }
--- app/Models/TaskStatus.php
+++ app/Models/TaskStatus.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class TaskStatus extends Model
 {
-    //
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
+
