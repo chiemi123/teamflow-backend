afe1fd5133dc029742464a327e1620e74cd8d49f
chiemi endo <milkrose0315@yahoo.co.jp>
2026-04-19 15:33:06 +0900

    feat: apply UserResource to auth endpoints and standardize user response format

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -21,7 +22,7 @@ class AuthController extends Controller
 
         return response()->json([
             'token' => $token,
-            'user' => $user
+            'user' => new UserResource($user)
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,11 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\ProjectController;
+use App\Http\Resources\UserResource;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return new UserResource($request->user());
 });
 
 Route::middleware('auth:sanctum')->group(function () {
