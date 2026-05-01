e66b591cefaa9a6c233f76f054f08183106d20e9
chiemi endo <milkrose0315@yahoo.co.jp>
2026-05-01 12:26:14 +0900

    fix: implement sanctum auth with nginx and production nextjs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,30 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->appendToGroup('api', [
+            EnsureFrontendRequestsAreStateful::class,
+        ]);
+
+        // セッションやクッキーに関するミドルウェアを追加
+        $middleware->appendToGroup('web', [
+            StartSession::class,   // セッションの開始
+            AddQueuedCookiesToResponse::class,  // クッキーをレスポンスに追加
+            EncryptCookies::class,  // セッションデータの暗号化
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
